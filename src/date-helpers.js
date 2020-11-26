cc1566b8c6002d36d217383d36f35a197808ef1c
cpears93 <cpearson3140@gmail.com>
2020-11-26 15:21:15 +0000

    Refactored code, added functionality

--- src/date-helpers.js
+++ src/date-helpers.js
@@ -2,4 +2,11 @@ const isWeekend = day => {
     console.log(day + `: ${day & 7}`);
 }
 
-export {isWeekend}
+const getDayName = day => {
+    const date = Date.now(Date.UTC(2018, 0, day));
+
+    return dayName = new Intl.DateTimeFormat("en-US", {weekday: "short"})
+    .format(date);
+}
+
+export {isWeekend, getDayName}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import { isWeekend } from "./date-helpers.js";
+import { isWeekend, getDayName } from "./date-helpers.js";
 
 const calendar = document.querySelector("app-calendar");
 
-
-
 for (let day = 1; day < 31; day++) {
-    console.log(day)
+    
+    const weekend = isWeekend(day);
 
-    const weekend = isWeekend(day)
+    let name = '';
+    if (day <  7) {
+        const dayTime = getDayName(day);
+        name = `div class="name">${dayName}</div>`;
 
-    calendar.insertAdjacentHTML("beforeend", `div class="day ${weekend ? "weekend" : ""}">${day}</div>`);
-}
+    }
+
+    const dayName = getDayName(day);
+
+    calendar.insertAdjacentHTML("beforeend", `div class="day ${weekend ? "weekend" : ""}">div class="name">
+    ${dayName}</div>${day}</div>`);
+} 
+
+document.querySelectorAll("#app-calendar .day").forEach(day => {
+    day.addEventListener("click", event => {
+        event.currentTarget.classList.toggle("selected");
+    }); 
+});
